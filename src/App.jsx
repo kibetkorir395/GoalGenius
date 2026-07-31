57aa570cf3e1ff8ff2b55f75a8e689ee24e1b6b9
coongames8 <coongames8@gmail.com>
2026-07-31 20:47:47 +0300

    new commit

--- src/App.jsx
+++ src/App.jsx
@@ -73,7 +73,7 @@ function App() {
       <Notification />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="subscribe" element={<ProtectedRoute>{/*currency === "KES" ? <FlutterwavePayment /> : <Pay />*/ <FlutterwavePayment /> }</ProtectedRoute>} />
+        <Route path="subscribe" element={<ProtectedRoute>{ currency === "KES" ? <Pay /> : <Payment /> }</ProtectedRoute>} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<ProtectedAuthRoute><Login /></ProtectedAuthRoute>} />
         <Route path="register" element={<ProtectedAuthRoute><Register /></ProtectedAuthRoute>} />
